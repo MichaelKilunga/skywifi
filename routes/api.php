6c20909f1d647a4745ebbeeec05b89fa30ab15c2
MICHAEL KILUNGA <mickworks30@gmail.com>
2025-09-18 19:30:39 +0300

    api

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,8 @@
 <?php
+Route::get('/check-auth', [PortalController::class, 'checkAuth']);
+
+// GET /api/user/{username}/mac/{mac}?action=authorize
+// GET /api/user/{username}/mac/{mac}?action=authenticate
+// POST /api/user/{username}/sessions/{session_id}?action=preacct
+// POST /api/user/{username}/sessions/{session_id}?action=accounting
+// POST /api/user/{username}/mac/{mac}?action=post-auth
